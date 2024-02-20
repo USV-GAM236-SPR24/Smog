ff71fecad6523249f44c8c191a72c30e76e0eccf
Ian Macaskill <ianmacaskill01@gmail.com>
2024-02-20 12:17:51 -0800

    Added script for mob tracking
    Short script that will path an attached node object that inherits from either KinematicBody2D or CharacterBody2D to the player node. Player node needs to be assigned at top of file using get_node().

---
+++ Smog/scripts/mob.gd
@@ -0,0 +1,15 @@
+#PATH is path to player node relative to live remote root
+# example: get_node("/root/Game/Player")
+@onready var player = get_node([PATH])
+
+@onready var mob_speed = 1
+
+func _physics_process(_delta):
+	#get direction from mob to player, factor in mob_speed
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * mob_speed
+	
+	#slides around collisions
+	move_and_slide()
+
+
