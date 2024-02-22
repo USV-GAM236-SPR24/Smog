562a9cb95139cb5deb56314c91c64e4540956c2d
Chai Nunes <chaiyawahoo@gmail.com>
2024-02-22 11:51:07 -0800

    int to float

--- Smog/scripts/sanity_bar.gd
+++ Smog/scripts/sanity_bar.gd
@@ -3,12 +3,19 @@ extends TextureProgressBar
 
 func _enter_tree() -> void:
 	Sanity.sanity_changed.connect(_on_sanity_changed)
+	value = Sanity.current
 
 
-func _on_sanity_changed(_old: int, new: int) -> void:
+func _on_sanity_changed(_old: float, new: float) -> void:
 	value = new
 
 
 func _input(event: InputEvent) -> void:
+	if event.is_released():
+		return
 	if event.is_action_pressed("ui_accept"):
 		Sanity.change(randi_range(0, 100))
+	elif event.is_action("ui_left"):
+		Sanity.decrease(step)
+	elif event.is_action("ui_right"):
+		Sanity.increase(step)
