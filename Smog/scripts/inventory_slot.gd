41640946754a04f2345c43164d67d9100142b94c
Chai Nunes <chaiyawahoo@gmail.com>
2024-02-26 02:11:00 -0800

    dragging

--- Smog/scripts/inventory_slot.gd
+++ Smog/scripts/inventory_slot.gd
@@ -8,20 +8,34 @@ var item_count: int = 0:
 	set(value):
 		item_count = value
 		_update_counter()
-var first_item: Item:
+@onready var items: Control:
 	get:
-		return %items.get_child(0)
+		return get_child(0)
+@onready var first_item: Item:
+	get:
+		return items.get_child(0)
+
 
 #is item stack selected?
 var selected = false
 
 var pressed = false
 
-@onready var collision_rect := Rect2(position, size*scale)
+var initial_items_position: Vector2
+var intial_label_position: Vector2
+
 
 func _process(delta):
 	if pressed:
-		global_position = get_global_mouse_position()
+		if initial_items_position == Vector2.ZERO:
+			initial_items_position = items.global_position
+			intial_label_position = %itemCounter.global_position
+		items.global_position = get_global_mouse_position() - get_global_rect().size / 2
+		%itemCounter.global_position = items.global_position + Vector2(10, -10)
+		return
+	if initial_items_position != Vector2.ZERO:
+		items.global_position = initial_items_position
+		%itemCounter.global_position = intial_label_position
 
 #call use() on item in %items and free it afterwards, then update counter
 func use() -> void:
@@ -41,7 +55,7 @@ func add_item(item: Item) -> void:
 	if item_count >= MAX_ITEMS:
 		return
 	
-	%items.add_child(item)
+	items.add_child(item)
 	item_count += 1
 
 
@@ -64,28 +78,36 @@ func toggle_selected() -> void:
 #update UI text
 func _update_counter() -> void:
 	%itemCounter.text = str(item_count)
+	%itemCounter.visible = item_count != 0
 
 
 #update UI on ready
 func _ready() -> void:
 	_update_counter()
-	print(collision_rect)
 
 
 func _on_button_button_down():
 	pressed = true
 
 func _on_button_button_up():
-	
-	var smallest: float = INF
-	var smallesSlot: InventorySlot = self
-	
 	if pressed:
 		for slot in get_parent().get_children():
-			var dist = self.global_position.distance_to(slot.global_position)
 			if self == slot:
 				continue
-			if dist < 20:
-				print(slot.name)
-				break
+			if slot.get_global_rect().has_point(get_global_mouse_position()):
+				var temp_parent = Control.new()
+				var temp_count = 0
+				temp_count = item_count
+				item_count = slot.item_count
+				slot.item_count = temp_count
+				for item in items.get_children():
+					items.remove_child(item)
+					temp_parent.add_child(item)
+				for item in slot.items.get_children():
+					slot.items.remove_child(item)
+					items.add_child(item)
+				for item in temp_parent.get_children():
+					temp_parent.remove_child(item)
+					slot.items.add_child(item)
+					
 	pressed = false
