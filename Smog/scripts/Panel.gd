e8f75c6be4710d49b095b47583410975b1a4a5d5
Ian Macaskill <ianmacaskill01@gmail.com>
2024-02-25 19:07:12 -0800

    Updated inventory
    Made Inventory smaller and added better temporary art (thanks artists). Also changed from  a click to use item system to one where you select an item by going left and right, then selecting use.

--- Smog/scripts/Panel.gd
+++ Smog/scripts/Panel.gd
@@ -2,7 +2,25 @@ extends Panel
 
 const MAX_ITEMS = 16
 
-@export var index : int
+#is item stack selected?
+var selected = false
+
+#call use() on item in %items and free it afterwards, then update counter
+func use():
+	if %items.get_child_count() > 0:
+		var item = %items.get_child(0)
+		item.use()
+		item.free()
+		_update_counter()
+	else:
+		print('Error: Panel.use(), No items to use!')
+		_update_counter()
+
+#adds items to the panel, unless more items are in panel than allowed
+func add_item(item : Item):
+	if get_child(0).get_child_count() + 1 <= MAX_ITEMS: #and item.get_parent() == null:
+		%items.add_child(item)
+	_update_counter()
 
 #if panel is full of items
 func full() -> bool:
@@ -11,30 +29,21 @@ func full() -> bool:
 	else:
 		return false
 
+#toggle selection visibility and local var
+func toggle_selected():
+	selected = not selected
+	%selectImg.visible = selected
+
 #update UI text
 func _update_counter():
-	%itemCounter.text = str(get_child(0).get_child_count())
-
+	%itemCounter.text = str(%items.get_child_count())
+	
 #update UI on ready
 func _ready():
 	_update_counter()
 
-#call use() on item in %items and free it afterwards, then update counter
-func use():
-	if %items.get_child_count() > 0:
-		var item = %items.get_child(0)
-		item.use()
-		item.queue_free()
-		_update_counter()
-	else:
-		_update_counter()
-
-#adds items to the panel, if more items are in panel than allowed
-func add_item(item : Item):
-	if get_child(0).get_child_count() + 1 <= MAX_ITEMS and item.get_parent() == null:
-		%items.add_child(item)
-	_update_counter()
-
-#use item on panel when button pressed
+#use item on panel when button pressed - REMOVED FEATURE FOR NOW
 func _on_button_pressed():
-	use()
+	#use()
+	pass
+
--- Smog/scripts/inventory.gd
+++ Smog/scripts/inventory.gd
@@ -2,27 +2,42 @@
 #    add_item(item : Item) -> void 
 #                     - to add an item to inventory
 #        -- IMPORTANT - Item must have ITEM_TYPE to determine stacking
-#                     - Item must have a use() function that applies its affects        
+#                     - Item must have a use() function that applies its affects      
+#
+#    use_item() -> void
+#                 - calls use() on currently selected item
+#  
 #    toggle() -> void 
 #                 - toggles visibility of the inventory system
 #
-#        -- IMPORTANT - To use an item, click on it in the UI
+#    move_selector_right() -> void
+#                 - moves selected item in inventory to the right
+#
+#    move_selector_left() -> void
+#                 - moves selected item in inventory to the left
+#   
+#          is_full() -> bool
+#                 - returns true if all slots are full, false otherwise
 extends Node2D
 
-var item_scene : PackedScene = preload("res://scenes/items/item.tscn")
-var item2_scene : PackedScene = preload("res://scenes/items/item_2.tscn")
+#example item scenes
+var item_scene : PackedScene = preload("res://scenes/temp/item.tscn")
+var item2_scene : PackedScene = preload("res://scenes/temp/item_2.tscn")
+
+#index of currently selected panel in panels
+var selectedPanelIndex = 0
 
 #list of inventory slots
 @onready var panels = %GridContainer.get_children()
 
-#toggle visibility
-func toggle() -> void:
-	self.visible = !self.visible
 
 ###  START TESTING
 # T - add one instance of item.tscn
 # F - add three instances of item2.tscn
 # G - example of using toggle() to toggle display of inventory
+# J - move_selector_left()
+# L - move_selector_right()
+# K - use currently selected item use_item()
 
 func _input(event : InputEvent) -> void:
 	# EXAMPLE : adding a single item from item.tscn
@@ -31,23 +46,41 @@ func _input(event : InputEvent) -> void:
 			var item_instance = item_scene.instantiate()
 			add_item(item_instance)
 		elif event.keycode == KEY_F: 
-		# EXAMPLE : Making three new instances from item2.tscn to add
 			for i in range(3):
 				var item2_instance = item2_scene.instantiate()
 				add_item(item2_instance)
-		#EXAMPLE : Using toggle()
 		elif event.keycode == KEY_G:
 			toggle()
+		elif event.keycode == KEY_L:
+			move_selector_right()
+		elif event.keycode == KEY_J:
+			move_selector_left()
+		elif event.keycode == KEY_K:
+			use_item()
+			
 #### END TESTING
 
-
-func _count_all_descendants(node : Node):
-	var count = 0 
-	for i in range(node.get_child_count()):
-		var child = node.get_child(i)
-		count += 1
-		count += _count_all_descendants(child) 
-	return count
+#toggle visibility
+func toggle() -> void:
+	self.visible = !self.visible
+	
+func move_selector_right() -> void:
+	panels[selectedPanelIndex].toggle_selected()
+	if(selectedPanelIndex + 1 < %GridContainer.get_child_count()):
+		panels[selectedPanelIndex + 1].toggle_selected()
+		selectedPanelIndex += 1
+	else:
+		selectedPanelIndex = 0
+		panels[selectedPanelIndex].toggle_selected()
+		
+func move_selector_left() -> void:
+	panels[selectedPanelIndex].toggle_selected()
+	if( selectedPanelIndex > 0 ):
+		panels[selectedPanelIndex - 1].toggle_selected()
+		selectedPanelIndex -= 1
+	elif selectedPanelIndex == 0:
+		selectedPanelIndex = %GridContainer.get_child_count() - 1
+		panels[selectedPanelIndex].toggle_selected()
 
 func is_full() -> bool:
 	var _full : bool = true
@@ -64,6 +97,8 @@ func add_item(item : Item) -> void:
 		print('Inventory full!')
 	else:
 		var found : bool = false
+		
+		#checking for similar stacks
 		for panel in panels:
 			#has item
 			if panel.get_child(0).get_child_count() > 0:
@@ -77,6 +112,17 @@ func add_item(item : Item) -> void:
 				if panel.get_child(0).get_child_count() == 0 and !foundNewSlot:
 					panel.add_item(item)
 					foundNewSlot = true
-					
-func use_item(index) -> void:
-	panels[index].use()
+func use_item() -> void:
+	panels[selectedPanelIndex].use()
+	
+#setup window size change signal and
+#set first slot as selected
+func _ready():
+	get_viewport().connect("size_changed", _on_window_resize)
+	panels[selectedPanelIndex].toggle_selected()
+	_on_window_resize()
+
+#center inventory on window resize
+func _on_window_resize() -> void:
+	var window_size = get_viewport().size
+	position = Vector2(window_size.x / 2, window_size.y - 20)
