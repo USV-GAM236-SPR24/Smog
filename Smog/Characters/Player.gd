999c3cd12efad3e5ddda1b23134622e5bc761040
EgrichSMG <158514079+EgrichSMG@users.noreply.github.com>
2024-02-26 17:35:13 -0800

    Update Player.gd

--- Smog/Characters/Player.gd
+++ Smog/Characters/Player.gd
@@ -7,7 +7,7 @@ func _physics_process(delta):
 	#get input direction
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("up") - Input.get_action_strength("down")
+		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
 	#print(input_direction)
@@ -15,5 +15,5 @@ func _physics_process(delta):
 	#update velocity
 	velocity = input_direction * player_move_speed
 	
-	#Move and Slider
+	#Move and Slide
 	move_and_slide()
