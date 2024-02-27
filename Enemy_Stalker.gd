a25e586a932fc6cfb8337dd86f1499a06bc3acde
Jonathan Escalera Ibarra <jonathanescaleraibarra@gmail.com>
2024-02-27 12:11:36 -0800

    Stalker
    kekw

--- Enemy_Stalker.gd
+++ Enemy_Stalker.gd
@@ -1,23 +1,37 @@
 extends CharacterBody2D
 
-var speed = 15
-var player_chase = false
+var speed = 10
+
+var player_chase = true
 " ^^ his is under the assumption that the stalker will have a 'stalk' phase and thus not always be
 the snail of death"
 
-var player = null
+
+@onready var player = $/root/Node2D/player
+
 
 func _physics_process(delta):
+	
+	velocity = speed*position.direction_to(player.position)
+	
+	if position.distance_to(player.position) < 20:
+		
+		player_chase = true
 	if player_chase:
-		position += (player.position - position)/speed 
+		#position += (player.position - position)/speed 
+		move_and_slide()
 
 
-func _on_detection_area_area_entered(body):
-	player = body
-	player_chase = true
+func _on_detection_area_body_entered(body):
+	print("Detected!")
+	if body.name == "player":
+		player = body
+		player_chase = true
 	pass # Replace with function body.
 "If player in zone, chase. If not, don't chase."
 func _on_detection_area_body_exited(body):
-	player = null
-	player_chase = false
+	if body.name == "player":
+		player = null
+		player_chase = false
+
 
--- player.gd
+++ player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
-const speed = 100
-"
-func _physics_process(delta):
-	pass
+var speed = 80
 
-func player_movement(delta):
-	
-"
+
+
+#deeznutz
+
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left","right","up","down")
+	velocity = direction*speed
+	move_and_slide()
