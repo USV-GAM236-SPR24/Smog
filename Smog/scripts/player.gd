08e5a5760f74446d8310eee5a6234a9cef9c8a8a
Ian Macaskill <ianmacaskill01@gmail.com>
2024-02-28 22:28:22 -0800

    Improved the player example
    No more moonwalking! Also a raycast exists that follows the mouse

--- Smog/scripts/player.gd
+++ Smog/scripts/player.gd
@@ -6,10 +6,12 @@ var direction
 var SPEED = 160
 
 func _physics_process(_delta) -> void:
+	_update_raycast()
+	
 	direction = Input.get_vector("left", "right", "up", "down")
 	
-	#exclude sideways movement
 	if !(direction.x == 0.0 or direction.x == 1.0 or direction.x == -1.0):
+		_update_anim()
 		return
 	
 	velocity = direction * SPEED
@@ -18,12 +20,16 @@ func _physics_process(_delta) -> void:
 	
 func _update_anim() -> void:
 	
-	if direction == Vector2.ZERO:
-		%AnimationPlayer.stop()
-		return
-	
 	match direction:
 		Vector2.DOWN: %AnimationPlayer.play("forwards")
 		Vector2.UP: %AnimationPlayer.play("up")
 		Vector2.RIGHT: %AnimationPlayer.play("right")
 		Vector2.LEFT: %AnimationPlayer.play("left")
+		_: %AnimationPlayer.stop()
+
+
+func _update_raycast():
+	%RayCast2D.target_position = get_global_mouse_position()
+	if %RayCast2D.is_colliding():
+		var collider = %RayCast2D.get_collider()
+		print(collider)
