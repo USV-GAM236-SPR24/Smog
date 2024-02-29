21a271254e40223b2409d038bb2cbb4674850597
MetaversePatriarch <147543390+MetaversePatriarch@users.noreply.github.com>
2024-02-28 22:24:25 -0800

    Prototype Update

---
+++ Smog/scenes/Player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+var raycast_length = 100  # Length of the raycast
+const SPEED = 300.0
+const JUMP_VELOCITY = -100.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	# Add the gravity.
+	#if not is_on_floor():
+		#velocity.y += gravity * delta
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept"):
+		#velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var directionX = Input.get_axis("left", "right")
+	var directionY = Input.get_axis("up", "down")
+	
+	if directionX:
+		velocity.x = directionX * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if directionY:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)	
+		
+	if Input.is_action_just_pressed("shoot") and $RayCast2D.is_colliding():
+		print("Player recognized Enemy")
+		var collider = $RayCast2D.get_collider()
+		collider._on_shoot()
+		
+	move_and_slide()
+		
