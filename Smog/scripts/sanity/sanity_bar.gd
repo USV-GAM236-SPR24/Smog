5dd4f3a63122efccd6eeab1edee931dc78b5ca37
Chai Nunes <chaiyawahoo@gmail.com>
2024-02-29 10:33:10 -0800

    Update sanity_bar.gd

--- Smog/scripts/sanity/sanity_bar.gd
+++ Smog/scripts/sanity/sanity_bar.gd
@@ -5,8 +5,10 @@ func _enter_tree() -> void:
 	Sanity.sanity_changed.connect(_on_sanity_changed)
 	value = Sanity.current
 
+
 func _on_sanity_changed(_old: float, new: float) -> void:
 	value = new
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_released():
