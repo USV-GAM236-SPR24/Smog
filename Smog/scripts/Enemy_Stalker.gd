910872560cc3db1b8d66cd17004ae7460dd40356
Jonathan Escalera Ibarra <jonathanescaleraibarra@gmail.com>
2024-03-05 10:44:55 -0800

    Stalker Damage
    Stalker does Damage now

--- Smog/scripts/Enemy_Stalker.gd
+++ Smog/scripts/Enemy_Stalker.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
 var speed = 10
+var damage = 25
+
+var draining = false
+var drain_tick_rate = 0.5
+var drain_tick_progress = drain_tick_rate
+
 
 var player_chase = true
-" ^^ his is under the assumption that the stalker will have a 'stalk' phase and thus not always be
-the snail of death"
+# ^^ his is under the assumption that the stalker will have a 'stalk' phase and thus not always be
+#the snail of death
 
 
-@onready var player = $/root/Node2D/player
+@onready var player = $/root/Game/Player
+
 
 
 func _physics_process(delta):
@@ -20,18 +27,27 @@ func _physics_process(delta):
 	if player_chase:
 		#position += (player.position - position)/speed 
 		move_and_slide()
+		
+	if draining:
+		drain_tick_progress += delta
+		if drain_tick_progress >= drain_tick_rate:
+			Sanity.decrease(damage)
+			drain_tick_progress -= drain_tick_rate
 
 
-func _on_detection_area_body_entered(body):
+func _on_area_2d_body_entered(body):
 	print("Detected!")
-	if body.name == "player":
+	if body.name == "Player":
 		player = body
 		player_chase = true
-	pass # Replace with function body.
-"If player in zone, chase. If not, don't chase."
-func _on_detection_area_body_exited(body):
-	if body.name == "player":
+		draining = true
+#If player in zone, chase. If not, don't chase.
+
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
 		player = null
 		player_chase = false
-
-
+		draining = false
+		drain_tick_progress = drain_tick_rate
