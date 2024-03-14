ebb00b1fe8f0f0b246357f755637426b3a452476
Chai Nunes <chaiyawahoo@gmail.com>
2024-03-14 01:48:45 -0700

    ldtk level

--- Smog/scripts/fodder.gd
+++ Smog/scripts/fodder.gd
@@ -2,6 +2,7 @@ class_name EnemyFodder
 extends Enemy
 
 
+var player: Player
 var player_chase = false
 var draining = false
 var drain_tick_rate = 0.5
@@ -17,8 +18,9 @@ func _physics_process(delta):
 	if health <= 0:
 		return
 	if player_chase:
-		velocity = position.direction_to(get_node("/root/Game/Player").position) * speed
-		$AnimatedSprite2D.play("walk")
+		if player:
+			velocity = position.direction_to(player.position) * speed
+			$AnimatedSprite2D.play("walk")
 	else:
 		velocity = Vector2.ZERO
 		$AnimatedSprite2D.play("idle")
@@ -31,24 +33,36 @@ func _physics_process(delta):
 
 
 func _on_detection_area_body_entered(body):
-	if body is Player:
-		player_chase = true
+	if not body is Player:
+		return
+	player_chase = true
+	if not player:
+		player = body
 
 
 func _on_detection_area_body_exited(body):
-	if body is Player:
-		player_chase = false
-	
+	if not body is Player:
+		return
+	player_chase = false
+	if not player:
+		player = body
+
 
 func _on_hitbox_body_entered(body):
-	if body is Player:
-		draining = true
-		
+	if not body is Player:
+		return
+	draining = true
+	if not player:
+		player = body
+
 
 func _on_hitbox_body_exited(body):
-	if body is Player:
-		draining = false
-		drain_tick_progress = drain_tick_rate
+	if not body is Player:
+		return
+	draining = false
+	drain_tick_progress = drain_tick_rate
+	if not player:
+		player = body
 
 
 func die():
--- Smog/scripts/interactables/pickup.gd
+++ Smog/scripts/interactables/pickup.gd
@@ -14,7 +14,7 @@ func _enter_tree() -> void:
 
 
 func _ready() -> void:
-	inventory = get_node("/root/Game/Inventory")
+	inventory = get_node("/root/Game/CanvasLayer/Inventory")
 	item = ItemFactory.create(interact_value)
 	interact_label = item.item_name.capitalize()
 	$Sprite2D.texture = item.texture
