97593a6e61fb5af20b07077fe41f8d3a60f4020d
CodinAround <102853784+CodinAround@users.noreply.github.com>
2024-03-14 11:27:55 -0700

    Added comment to script

--- Smog/scenes/Area2D.gd
+++ Smog/scenes/Area2D.gd
@@ -4,6 +4,7 @@ signal trainShown
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#change "mouse_entered "to "Area_2D enter" when a new level is called
 	connect("mouse_entered", _showTrain)
 
 func _showTrain() -> void:
