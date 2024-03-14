77344cfa5c05d92f9dacbd7bca6aed6394f88ef4
Chai Nunes <chaiyawahoo@gmail.com>
2024-03-13 18:23:24 -0700

    discoverables update

--- Smog/scripts/discoverable.gd
+++ Smog/scripts/discoverable.gd
@@ -1,36 +1,39 @@
 class_name Discoverable
 extends Interactable
 
-
 enum {
 	TEXT,
 	DIALOGUE,
 	IMAGE
 }
 
+@export_enum("office1", "mailroom", "messhall", "factory_floor1", "factory_floor2", "washroom", "storage", "office2")
+var room: String = "office1"
 @export var key: String
 
 var display_name: String
 var text: String
 var type: int
 var texture: Texture2D
+var texture_path: String
 var image: Texture2D
+var image_path: String
 var has_image: bool
 var seen: bool
 
 @onready var sprite: Sprite2D = $Sprite2D
 
 
-func _init(new_name := "Empty Discoverable", new_text := "Empty Text", new_type := Discoverable.TEXT, new_texture: Texture2D = null, new_image: Texture2D = null) -> void:
+func _init(new_name := "Empty Discoverable", new_text := "Empty Text", new_type := Discoverable.TEXT, new_texture: String = "", new_image: String = "") -> void:
 	display_name = new_name
 	text = new_text
 	type = new_type
-	texture = new_texture
-	image = new_image
-	has_image = new_image != null
+	texture_path = new_texture
+	image_path = new_image
+	has_image = type == Discoverable.IMAGE
 	seen = false
 	
-	if texture == null:
+	if texture_path == "":
 		texture = PlaceholderTexture2D.new()
 		texture.size = Vector2(16, 16)
 	
@@ -40,6 +43,10 @@ func _init(new_name := "Empty Discoverable", new_text := "Empty Text", new_type 
 
 
 func _ready() -> void:
+	if texture_path != "":
+		texture = ResourceLoader.load_threaded_get(texture_path)
+	if has_image:
+		image = ResourceLoader.load_threaded_get(image_path)
 	sprite.texture = texture
 
 
--- Smog/scripts/discoverable_system.gd
+++ Smog/scripts/discoverable_system.gd
@@ -108,3 +108,42 @@ var office2_handkerchief := Discoverable.new(
 )
 #endregion
 
+var discoverables := {}
+
+
+func _init():
+	_parse_json()
+
+
+func _parse_json() -> void:
+	var file: FileAccess = FileAccess.open("res://discoverables.json", FileAccess.READ)
+	var rooms: Dictionary = JSON.parse_string(file.get_as_text())
+	for room_name in rooms:
+		var room: Dictionary = rooms[room_name]
+		discoverables[room_name] = {}
+		for key in room:
+			discoverables[room_name].display_name = room[key].display_name
+			discoverables[room_name].text = room[key].text
+			discoverables[room_name].type = _string_type(room[key].type)
+			discoverables[room_name].texture_path = room[key].texture_path
+			if room[key].texture_path != "":
+				if not ResourceLoader.load_threaded_get_status(room[key].texture_path):
+					ResourceLoader.load_threaded_request(room[key].texture_path)
+					var status: int = ResourceLoader.load_threaded_get_status(room[key].texture_path)
+					print_debug("%s: %s" % [status, room[key].texture_path])
+			if room[key].type != "IMAGE":
+				continue
+			discoverables[room_name].image_path = room[key].image_path
+			if room[key].image_path != "":
+				if not ResourceLoader.load_threaded_get_status(room[key].image_path):
+					ResourceLoader.load_threaded_request(room[key].image_path)
+					var status: int = ResourceLoader.load_threaded_get_status(room[key].image_path)
+					print_debug("%s: %s" % [status, room[key].image_path])
+
+
+func _string_type(typename: String) -> int:
+	match typename:
+		"TEXT": return Discoverable.TEXT
+		"IMAGE": return Discoverable.IMAGE
+		"DIALOGUE": return Discoverable.DIALOGUE
+	return -1
