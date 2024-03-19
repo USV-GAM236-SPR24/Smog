af371b2ac51acd83cbb46e827918e36d52ca99dc
Chai Nunes <chaiyawahoo@gmail.com>
2024-03-19 10:26:21 -0700

    weapon bases

---
+++ Smog/scripts/weapon/ranged_weapon.gd
@@ -0,0 +1,9 @@
+class_name RangedWeapon
+extends Weapon
+
+
+var range: int
+var ammo_capacity: int
+#var ammo_type: String
+
+var current_ammo: int
---
+++ Smog/scripts/weapon/weapon.gd
@@ -0,0 +1,8 @@
+class_name Weapon
+extends Node
+
+
+@export var sprite_frames: SpriteFrames
+
+var weapon_name: String
+var damage: int
