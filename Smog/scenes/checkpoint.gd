388d6dcbb95cf79686d53a077207b98c0d4683d0
Joshua Buckmann <145380891+JBuck55@users.noreply.github.com>
2024-03-26 11:31:08 -0700

    First Checkpoint Draft

--- Smog/scenes/checkpoint.gd
+++ Smog/scenes/checkpoint.gd
@@ -1,4 +1,9 @@
 extends Node2D
- 
-func _ready():
-	 
+
+@export var checkpoint_name : String
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		SaveSystem.set_var("checkpoint " + checkpoint_name, true)
+		SaveSystem.save()
